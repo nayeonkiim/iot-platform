164a8af6d279ca874ef013973765667976a9191b
nayeonkiim <wkdrnsldk98@naver.com>
2021-12-05 11:47:35 +0900

    add graph

--- chartjsintro.js
+++ chartjsintro.js
@@ -1,13 +1,17 @@
 "use strict"
 
-
 function line(app, amount) {
-    invokeAPI(convertDate(amount), app);
+    invokeAPILine(convertDate(amount), app);
+}
+
+function bar(app, amount) {
+    invokeAPIBar(convertDate(amount), app);
 }
 
 var convertDate = function (amount) {
     let firstDay;
     let lastDay;
+    let plusGraph;
 
     var now = new Date();
     console.log("amount:" + amount);
@@ -20,6 +24,7 @@ var convertDate = function (amount) {
         console.log("일주일 온도")
         firstDay = new Date(now.getFullYear(), now.getMonth());
         lastDay = new Date(now.getFullYear(), now.getMonth() + 1);
+        plusGraph = 1;
     }
 
     var arr = [];
@@ -44,16 +49,16 @@ var convertDate = function (amount) {
     return arr;
 }
 
-var invokeAPI = function (arr, app) {
+var invokeAPILine = function (arr, app) {
 
     // 디바이스 조회 URI
     // prod 스테이지 편집기의 맨 위에 있는 "호출 URL/devices"로 대체해야 함
-    var API_URI = 'your api uri';
+    var API_URI = 'https://ra32sl0gc1.execute-api.ap-northeast-2.amazonaws.com/prod/devices/DeviceData/log?from=' + arr[0] + '&to=' + arr[1];
 
     axios.get(API_URI)
         .then(response => {
             var chartData = {};
-            chartData.LondonMonthlyTemperatures = {
+            chartData.datas = {
                 timestamp: [],
                 temperature: [],
                 humidity: [],
@@ -73,21 +78,21 @@ var invokeAPI = function (arr, app) {
 
             for (var k = 0; k < result.data.length; k++) {
                 //var splitData = jsonDatas[k].timestamp.split(' ');
-                chartData.LondonMonthlyTemperatures.timestamp[k] = jsonDatas[k].timestamp;
-                chartData.LondonMonthlyTemperatures.temperature[k] = jsonDatas[k].temperature;
-                chartData.LondonMonthlyTemperatures.humidity[k] = jsonDatas[k].humidity;
+                chartData.datas.timestamp[k] = jsonDatas[k].timestamp;
+                chartData.datas.temperature[k] = jsonDatas[k].temperature;
+                chartData.datas.humidity[k] = jsonDatas[k].humidity;
             }
             return chartData;
         })
         .then((chartData) => {
             const data =
             {
-                labels: chartData.LondonMonthlyTemperatures.timestamp,
+                labels: chartData.datas.timestamp,
                 datasets: [{
                     label: 'temperature',
                     backgroundColor: chartjsColors.maxTempLine,
                     borderColor: chartjsColors.maxTempLine,
-                    data: chartData.LondonMonthlyTemperatures.temperature,
+                    data: chartData.datas.temperature,
                     borderWidth: 1,
                     tension: 0.4,
                 },
@@ -95,7 +100,7 @@ var invokeAPI = function (arr, app) {
                     label: 'humidity',
                     backgroundColor: chartjsColors.minTempLine,
                     borderColor: chartjsColors.minTempLine,
-                    data: chartData.LondonMonthlyTemperatures.humidity,
+                    data: chartData.datas.humidity,
                     borderWidth: 1,
                     tension: 0.4,
                 },]
@@ -137,7 +142,7 @@ var invokeAPI = function (arr, app) {
                         title:
                         {
                             display: true,
-                            text: 'London Monthly Temperatures (\u00B0C)'
+                            text: '농작물 알맞은 온습도조회 (\u00B0C)'
                         }
                     }
                 }
@@ -149,3 +154,95 @@ var invokeAPI = function (arr, app) {
         .catch(e => console.log(e));
 }
 
+
+var invokeAPIBar = function (arr, app) {
+
+    // 디바이스 조회 URI
+    // prod 스테이지 편집기의 맨 위에 있는 "호출 URL/devices"로 대체해야 함
+    var API_URI = 'https://ra32sl0gc1.execute-api.ap-northeast-2.amazonaws.com/prod/devices/DeviceData/log?from=' + arr[0] + '&to=' + arr[1];
+
+    axios.get(API_URI)
+        .then(response => {
+            var chartData = {};
+            chartData.datas = {
+                timestamp: [],
+                temperature: [],
+                humidity: [],
+            };
+
+            console.log("response: " + response.data);
+            var result = JSON.parse(response.data);
+            var jsonDatas = [];
+            for (var i in result.data) {
+                jsonDatas.push(result.data[i]);
+            }
+
+            console.log(jsonDatas);
+            jsonDatas.sort(function (a, b) {
+                return Date.parse(a.timestamp) - Date.parse(b.timestamp);
+            });
+
+            var avgArr = [];
+            var count = 0;
+            var avg = 0;
+            var beforeDate;
+            var avgDate = [];
+            
+            for(var k=0; k < avgArr.size(); k++) {
+                var mydate = chartData.datas[k].timestamp.split(' ')[0];
+                if(k != 0 && mydate == beforeDate) {
+                    avg += chartData.datas[k].temperature;
+                    count += 1;
+                }else{
+                    if(k !=0) {
+                        avgArr.push(avg/count);
+                        avgDate.push(beforeDate);
+                    }
+                    avg = charData.datas[k].temperature;
+                    count = 1;
+                    beforeDate = mydate;
+                }
+
+                if(k == avgArr.size()-1) {
+                    avgArr.push(avg/count);
+                    avgDate.push(mydate);
+                }
+            }
+
+            for (var k = 0; k < result.data.length; k++) {
+                chartData.datas.timestamp[k] = avgDate[k];
+                chartData.datas.temperature[k] = avgArr[k];
+            }
+            return chartData;
+        })
+        .then((chartData) => {
+            const myChart = new Chart(app.canvasContext,
+                {
+                    type: 'bar',
+                    data:
+                    {
+                        labels: chartData.countryPopulations.countries,
+                        datasets: [{
+                            label: 'temperature',
+                            backgroundColor: chartjsColors.maxTempLine,
+                            borderColor: chartjsColors.maxTempLine,
+                            data: chartData.datas.temperature,
+                            borderWidth: 1,
+                            tension: 0.4,
+                        }]
+                    },
+                    options:
+                    {
+                        plugins:
+                        {
+                            legend: { display: false, },
+                            title: { display: true, text: 'Population (millions)' }
+                        },
+                        // indexAxis: 'y',
+                        scales: { y: { beginAtZero: true } }
+                    }
+                });
+    
+        })
+        .catch(e => console.log(e));
+}
