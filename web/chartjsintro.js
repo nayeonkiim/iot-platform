40def2b0c2a955077201dde9c546300d56e08e2e
nayeonkiim <wkdrnsldk98@naver.com>
2021-12-05 14:11:18 +0900

    그래프 수정

--- web/chartjsintro.js
+++ web/chartjsintro.js
@@ -53,7 +53,7 @@ var invokeAPILine = function (arr, app) {
 
     // 디바이스 조회 URI
     // prod 스테이지 편집기의 맨 위에 있는 "호출 URL/devices"로 대체해야 함
-    var API_URI = 'https://ra32sl0gc1.execute-api.ap-northeast-2.amazonaws.com/prod/devices/DeviceData/log?from=' + arr[0] + '&to=' + arr[1];
+    var API_URI = 'your api';
 
     axios.get(API_URI)
         .then(response => {
@@ -158,7 +158,7 @@ var invokeAPIBar = function (arr, app) {
 
     // 디바이스 조회 URI
     // prod 스테이지 편집기의 맨 위에 있는 "호출 URL/devices"로 대체해야 함
-    var API_URI = 'https://ra32sl0gc1.execute-api.ap-northeast-2.amazonaws.com/prod/devices/DeviceData/log?from=' + arr[0] + '&to=' + arr[1];
+    var API_URI = 'your api';
 
     axios.get(API_URI)
         .then(response => {
